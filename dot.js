a27fa3557be9cf2f6966192bbb26a2a81a9b69a6
dextercallender <dec2148@columbia.edu>
2020-03-30 21:04:49 -0400

    more scaffolding

--- dot.js
+++ dot.js
@@ -1,7 +1,25 @@
-function setup() {
+let cnv;
 
+let colors;
+
+function setup() {
+	createCanvas(400, 400);
+
+	colors = new Color();
+	var gui = new dat.GUI();
+	gui.add(colors, 'g', 0, 255);
 }
 
 function draw() {
+	cnv = createCanvas(windowWidth, windowHeight)
+  cnv.parent('dot-tool-canvas');
+	fill(colors.r, colors.g, colors.b);
+	ellipse(20, 20, 20, 20);
 
+}
+
+function Color() {
+  this.r = 255;
+  this.g = 100;
+  this.b = 255;
 }
